d2ccddd5f259c8ecc7989e83f4e092ae1df99162
LewisSaber <39595617+LewisSaber@users.noreply.github.com>
2023-03-06 13:01:04 +0200

    Add files via upload

---
+++ Input.js
@@ -0,0 +1,45 @@
+import Gui from "./Gui.js";
+import { mergeObject } from "./Utility.js";
+
+
+
+export default class Input extends Gui {
+
+    constructor() {
+        super()
+        this.type = "text"
+
+    }
+    createHTMLElement() {
+        this.container = document.createElement("input")
+        this.container.type = this.type
+        this.container.innerHTML = ""
+    }
+
+    setType(type) {
+        this.type = type
+        if (this.isBuilt) {
+            this.container.type = this.type
+        }
+        return this
+    }
+    onchange(evt) {
+        this.dispatchEvent("change", evt)
+    }
+    addListeners() {
+        super.addListeners()
+        this.container.addEventListener("change", (evt) => { this.onchange(evt) })
+
+    }
+
+    getFile() {
+        if (this.isBuilt)
+            if (this.type == "file") {
+                return this.container.files[0]
+            }
+        return false
+    }
+
+
+
+}
